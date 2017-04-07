98f86cdbe403ffff2be0c0dfce761e005ea196df
yamapi06150 <yamapi06150@gmail.com>
2017-04-08 04:21:32 +0800

    complete basic requirement

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
 const vm = new Vue({
    el: '#app', 
    data: {
-       msg: 'yoyoyo' 
+       msg: 'yoyoyo' ,
+       testVModelWithInput: 'yo mate' ,
+       todoText:'',
+       todos: [
+           { title: 'Todo 1', checked: true },
+           { title: 'Todo 2', checked: false },
+           { title: 'Todo 3', checked: false },
+           { title: 'Todo 4', checked: true },
+           { title: 'Todo 5', checked: true }
+       ]
+   },
+   methods: {
+       addTodo: function(){
+            console.log(this.todoText);
+            const todoText = this.todoText.trim();
+            if(todoText){
+                this.todos.push( { title: todoText, checked: false } );    
+                this.todoText='';
+            }
+            // todos.push({ 
+            //     id: todos.length-1,
+            //     title: title, 
+            //     checked: false
+            // });
+       },
+       remove: function(id){
+           this.todos.splice(id, 1);
+       },
+       filterForChecked: function(){
+           return this.todos.filter(function(todo){
+                return todo.checked;
+           }).length ;
+       }
    }
 });
